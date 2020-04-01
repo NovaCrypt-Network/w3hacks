cd1c7a1d2e8539d5acb8913eec769cdb982f352b
Calix Huang <calix.huang1@gmail.com>
2020-03-31 22:35:45 -0700

    added admin route for admin app

--- admin/urls.py
+++ admin/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url('admin/', admin.site.urls),
     url("^$", views.index, name="index"),
 ]
