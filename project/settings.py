58237a8798537be5d1d54e6718403b727662f269
Calix Huang <calix.huang1@gmail.com>
2020-03-31 22:11:41 -0700

    connected django project to postgresql

--- project/settings.py
+++ project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'w3hacks',
+        'USER': 'w3hacks_user',
+        'PASSWORD': 'w3hacks_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
