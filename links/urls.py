cb76f7c2b0dbdbb7194d8efb5d131b6a86445622
Calix Huang <calix.huang1@gmail.com>
2020-04-02 16:17:57 -0700

    added dynamic resource link functionality

--- links/urls.py
+++ links/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("^$", views.index, name="index"),
+    url("^(?P<url_extension>[^/]+)/$", views.link, name="link"),
 ]
--- links/views.py
+++ links/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from main.models import ResourceLink
 
-def index(request):
-    return HttpResponseRedirect("https://www.calix.dev/")
+def link(request, url_extension):
+    # Link exists
+    if ResourceLink.objects.filter(url_extension=url_extension).exists():
+        resource_link = ResourceLink.objects.get(url_extension=url_extension)
+        return HttpResponseRedirect(resource_link.link)
+        
+    # Link doesn't exist
+    else:
+        return HttpResponse("That link doesn't exist.")
