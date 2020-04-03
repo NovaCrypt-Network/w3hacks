819608edc9ea90beee87d1c03acdda49a7ed5243
Calix Huang <calix.huang1@gmail.com>
2020-04-03 16:19:53 -0700

    wired together staff pages

--- staff/urls.py
+++ staff/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url("^$", views.index, name="index"),
+    url("^hackathons/$", views.hackathons, name="hackathons"),
+    url("^edit-hackathon/$", views.edit_hackathon, name="edit_hackathon"),
+    url("^create-hackathon/$", views.create_hackathon, name="create_hackathon"),
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
 
 def index(request):
-    return render(request, "staff/index.html")
+    return HttpResponseRedirect("/hackathons/")
+
+def hackathons(request):
+    return render(request, "staff/hackathons.html")
+
+
+def edit_hackathon(request):
+    return render(request, "staff/edit-hackathon.html")
+
+
+def create_hackathon(request):
+    return render(request, "staff/create-hackathon.html")
