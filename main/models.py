f4b55fe5d34d8cefec09b73da0b06ef3ae805ffa
Calix Huang <calix.huang1@gmail.com>
2020-04-05 11:48:11 -0700

    removed certain fields and models

--- main/models.py
+++ main/models.py
@@ -19,12 +19,16 @@ class Profile(models.Model):
     profile_picture = models.ImageField(null=True, blank=True) # OPTIONAL: A profile picture for the user
     skills = ArrayField(models.CharField(max_length=50), null=True, blank=True) # OPTIONAL: An array of the user's skills
     social_links = models.ManyToManyField("SocialLink", blank=True) # OPTIONAL: A list of social links for the user
-    past_non_w3hacks_hackathons = models.ManyToManyField("PastHackathon", blank=True) # OPTIONAL: A list of past hackathons that the user has competed in outside of w3Hacks
     past_hackathons = models.ManyToManyField("Hackathon", blank=True) # OPTIONAL: A lit of past w3Hacks hackathons that the user has competed in
     projects = models.ManyToManyField("Project", blank=True) # List of projects created by user
     # followers = models.ManyToManyField("Profile", blank=True) # OPTIONAL: A list of profiles that follow self user
     # following = models.ManyToManyField("Profile", blank=True) # OPTIONAL: A list of profiles that this user follows
     joined_date = models.DateField() # The date when the user joined w3Hacks
+    credits = models.IntegerField() # The number of credits the user has
+    overall_ranking_points = models.IntegerField() # The overall ranking points the user has
+    project_ranking_points = models.IntegerField() # The project ranking points the user has
+    quiz_ranking_points = models.IntegerField() # The quiz ranking points the user has
+    exercise_ranking_points = models.IntegerField() # The exercise ranking points the user has
 
     def __str__(self):
         return self.user.username
@@ -43,7 +47,6 @@ class Project(models.Model):
     extra_files = ArrayField(models.FileField(), null=True, blank=True) # OPTIONAL: Array of extra files to submit along with project
     creator = models.ForeignKey("Profile", on_delete=models.PROTECT) # Creator of project
     likes = models.IntegerField(null=True, blank=True) # OPTIONAL: Number of likes on project
-    comments = models.ManyToManyField("Comment", blank=True) # OPTIONAL: List of comments on project
 
     def __str__(self):
         return self.title
@@ -114,21 +117,3 @@ class ScheduleEvent(models.Model):
 
     def __str__(self):
         return self.title
-
-
-# For each past hackathon that a user has competed in outside of w3Hacks
-class PastHackathon(models.Model):
-    title = models.CharField(max_length=50) # Name of the hackathon
-    description = models.TextField(max_length=300) # Description of the hackathon
-    start_date = models.DateField() # Starting date of the hackathon
-    end_date = models.DateField() # Ending date of the hackathon
-    awards = models.CharField(max_length=100, null=True, blank=True) # OPTIONAL: Awards that the user won
-
-
-# A comment model for each project
-class Comment(models.Model):
-    user = models.ForeignKey("Profile", on_delete=models.PROTECT) # User who created the comment
-    content = models.TextField(max_length=300) # Content of the comment
-
-    def __str__(self):
-        return self.user.user.username
