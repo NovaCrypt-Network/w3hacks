700b625c795ee76c1ce9416aa3e67b17e1a4eff3
Calix Huang <calix.huang1@gmail.com>
2020-04-07 15:04:53 -0700

    setup and configured staff app and subdomain

--- project/hosts.py
+++ project/hosts.py
@@ -6,5 +6,6 @@ host_patterns = patterns('',
     host(r'api', 'api.urls', name='api'),
     host(r'app', 'app.urls', name='app'),
     host(r'hackathon', 'hackathon.urls', name='hackathon'),
+    host(r'staff', 'staff.urls', name='staff'),
     host(r'links', 'links.urls', name='links'),
 )
--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'app',
     'hackathon',
     'links',
+    'staff',
     'main',
 ]
 
