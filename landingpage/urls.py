9e16122dfead8abb0200a2f619a6b56b6e900909
Calix Huang <calix.huang1@gmail.com>
2020-04-12 09:09:05 -0700

    setup for production

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -23,25 +23,25 @@ urlpatterns = [
     # url("^(?P<hackathon_id>[^/]+)/awards/$", hackathonViews.awards, name="awards"),
 
     # App views
-    url("^achievements/$", appViews.achievements, name="achievements"),
-    url("^leaderboards/$", appViews.leaderboards, name="leaderboards"),
-
-    # Activities URL routes
-    url("^activites/$", appViews.activites, name="activites"),
-    url("^project-exercises/$", appViews.project_exercises, name="project_exercises"),
-    url("^quiz-exercises/$", appViews.quiz_exercises, name="quiz_exercises"),
-    url("^mini-exercises/$", appViews.mini_exercises, name="mini_exercises"),
-
-    # Hackathon views
-    url("^hackathon/$", appViews.hackathon, name="hackathon"),
-    url("^about-the-hackathon/$", appViews.about_the_hackathon, name="about_the_hackathon"),
-    url("^past-hackathons/$", appViews.past_hackathons, name="past_hackathons"),
-    url("^future-hackathons/$", appViews.future_hackathons, name="future_hackathons"),
-
-    # Profile views
-    url("^profile/(?P<user_id>[^/]+)/$", appViews.profile, name="profile"),
-    url("^edit-profile/(?P<user_id>[^/]+)/$", appViews.edit_profile, name="edit_profile"),
-    url("^logout/$", appViews.user_logout, name="logout"),
+    # url("^achievements/$", appViews.achievements, name="achievements"),
+    # url("^leaderboards/$", appViews.leaderboards, name="leaderboards"),
+    #
+    # # Activities URL routes
+    # url("^activites/$", appViews.activites, name="activites"),
+    # url("^project-exercises/$", appViews.project_exercises, name="project_exercises"),
+    # url("^quiz-exercises/$", appViews.quiz_exercises, name="quiz_exercises"),
+    # url("^mini-exercises/$", appViews.mini_exercises, name="mini_exercises"),
+    #
+    # # Hackathon views
+    # url("^hackathon/$", appViews.hackathon, name="hackathon"),
+    # url("^about-the-hackathon/$", appViews.about_the_hackathon, name="about_the_hackathon"),
+    # url("^past-hackathons/$", appViews.past_hackathons, name="past_hackathons"),
+    # url("^future-hackathons/$", appViews.future_hackathons, name="future_hackathons"),
+    #
+    # # Profile views
+    # url("^profile/(?P<user_id>[^/]+)/$", appViews.profile, name="profile"),
+    # url("^edit-profile/(?P<user_id>[^/]+)/$", appViews.edit_profile, name="edit_profile"),
+    # url("^logout/$", appViews.user_logout, name="logout"),
 ]
 
 urlpatterns += [
