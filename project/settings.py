ec517063b7d7189390ba0325b178784a39d96a81
Calix Huang <calix.huang1@gmail.com>
2020-04-20 17:24:10 -0700

    fixed whitenoise thing

--- project/settings.py
+++ project/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
