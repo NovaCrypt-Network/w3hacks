52e4e600a24ed8368e7784b7d8603872b13f8cf8
Calix Huang <calix.huang1@gmail.com>
2020-04-21 13:35:38 -0700

    made app semi responsive

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -8,7 +8,7 @@ from app import views as appViews
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url("^$", views.index, name="index"),
+    # url("^$", views.index, name="index"),
     url("^about/$", views.about, name="about"),
     url("^contact/$", views.contact, name="contact"),
     url("^login/$", views.user_login, name="login"),
