5b4b987959db0a6fc8b6715bd84bc5c9df4f91ea
Calix Huang <calix.huang1@gmail.com>
2020-07-02 23:49:04 -0700

    added links

--- links/urls.py
+++ links/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url("^$", views.index, name="index"),
     url("^(?P<url_extension>[^/]+)/$", views.link, name="link"),
 ]
--- links/views.py
+++ links/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from main.models import ResourceLink
 
+def index(request):
+    links = ResourceLink.objects.all()
+    return render(request, "links/index.html", context={
+        "links": links
+    })
+
 def link(request, url_extension):
     # Link exists
     if ResourceLink.objects.filter(url_extension=url_extension).exists():
         resource_link = ResourceLink.objects.get(url_extension=url_extension)
         return HttpResponseRedirect(resource_link.link)
-        
+
     # Link doesn't exist
     else:
         return HttpResponse("That link doesn't exist.")
