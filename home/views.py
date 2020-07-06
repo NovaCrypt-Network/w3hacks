39a920e6d3eae2c80712956afe49e1b1c2f25ca5
Calix Huang <calix.huang1@gmail.com>
2020-07-06 10:24:34 -0700

    setup landingpage views

--- home/views.py
+++ home/views.py
@@ -6,39 +6,24 @@ from home import models
 
 
 def index(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect("/dashboard/")
-
-    if request.method == "POST":
-        name = request.POST.get("name")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
-
-        email_body = f"From: {name}\nEmail: {email}\n\n{message}"
-        EmailMessage("w3Hacks Contact Us", email_body, to=["calix.huang1@gmail.com"]).send()
-
-        return render(request, "home/index.html", context={
-            "message": "Message sent!"
-        })
-
-    return render(request, "landingpage/index.html")
+    return render(request, "home/index.html")
 
 
 def contact(request):
-    return render(request, "landingpage/contact.html")
+    return render(request, "home/contact.html")
 
 
 def team(request):
-    return render(request, "landingpage/team.html")
+    return render(request, "home/team.html")
 
 
 def join(request):
-    return render(request, "landingpage/join.html")
+    return render(request, "home/join.html")
 
 
 def events(request):
     events = models.Event.objects.all().order_by('-datetime')
-    return render(request, "landingpage/events.html", context={
+    return render(request, "home/events.html", context={
         "events": events
     })
 
@@ -46,7 +31,7 @@ def events(request):
 def event(request, event_url):
     if models.Event.objects.filter(url_extension=event_url).exists():
         event = models.Event.objects.get(url_extension=event_url)
-        return render(request, "landingpage/event.html", context={
+        return render(request, "home/event.html", context={
             "event": event
         })
 
@@ -59,21 +44,21 @@ def event(request, event_url):
 
 def news(request):
     updates = models.NewsUpdate.objects.all().order_by('-date_posted')
-    return render(request, "landingpage/news.html", context={
+    return render(request, "home/news.html", context={
         "updates": updates
     })
 
 
 def blog(request):
     blogs = models.BlogPost.objects.all().order_by('-date_posted')
-    return render(request, "landingpage/blog.html", context={
+    return render(request, "home/blog.html", context={
         "blogs": blogs,
     })
 
 def blog_post(request, blog_url):
     if models.BlogPost.objects.filter(url_extension=blog_url).exists():
         blog = models.BlogPost.objects.get(url_extension=blog_url)
-        return render(request, "landingpage/blog-post.html", context={
+        return render(request, "home/blog-post.html", context={
             "blog": blog
         })
 
