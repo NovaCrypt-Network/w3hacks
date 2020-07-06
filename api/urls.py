1321858cf77d28e796bffc42df22e99de17f7e2c
Calix Huang <calix.huang1@gmail.com>
2020-07-06 10:51:25 -0700

    removed api

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url("^$", views.index, name="index"),
-    url("^add-competitor-to-hackathon/$", views.add_competitor_to_hackathon, name="add_competitor_to_hackathon"),
-]
