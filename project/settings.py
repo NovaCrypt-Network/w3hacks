fc0702e52f420974d0ae83e1a541b9806f43bc60
Calix Huang <calix.huang1@gmail.com>
2020-07-05 20:03:47 -0700

    generated staticfiles

--- project/settings.py
+++ project/settings.py
@@ -23,6 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "test"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -145,6 +146,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Media Files
