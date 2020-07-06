a55df67661323ac70b9fca53cbb82ec9283efcc6
Calix Huang <calix.huang1@gmail.com>
2020-07-06 10:02:35 -0700

    missing migration

--- project/settings.py
+++ project/settings.py
@@ -22,8 +22,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECRET_KEY = "test"
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "test"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
